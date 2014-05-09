2b75e20855c3cd5c1b7e599710999f2a01e075ad
Kevin Wang <kevin.wang@rmp.uhn.on.ca>
2014-05-09 13:58:59 +0000

    Re #591: Initial commit of the module. Todo: need to change the node name to make it more readable.

---
+++ src/SequenceResample/SequenceResample.py
@@ -0,0 +1,377 @@
+import os
+import unittest
+from __main__ import vtk, qt, ctk, slicer
+
+#
+# SequenceResample
+#
+
+class SequenceResample:
+  def __init__(self, parent):
+    import string
+    parent.title = "Sequence Resample"
+    parent.categories = ["Multidimensional data"]
+    parent.dependencies = []
+    parent.contributors = ["Kevin Wang (PMH)"] # replace with "Firstname Lastname (Org)"
+    parent.helpText = """
+    This is a development module for label statistics of sequence data.
+    """
+    parent.acknowledgementText = """
+    This file was originally developed by Kevin Wang and was funded by PMH and OCAIRO.
+""" # replace with organization, grant and thanks.
+    self.parent = parent
+    try:
+      slicer.selfTests
+    except AttributeError:
+      slicer.selfTests = {}
+    slicer.selfTests['SequenceResample'] = self.runTest
+
+  def runTest(self):
+    tester = SequenceResampleTest()
+    tester.runTest()
+
+#
+# qSequenceResampleWidget
+#
+
+class SequenceResampleWidget:
+  def __init__(self, parent = None):
+    if not parent:
+      self.parent = slicer.qMRMLWidget()
+      self.parent.setLayout(qt.QVBoxLayout())
+      self.parent.setMRMLScene(slicer.mrmlScene)
+    else:
+      self.parent = parent
+    self.layout = self.parent.layout()
+    if not parent:
+      self.setup()
+      self.parent.show()
+
+  def setup(self):
+    # Instantiate and connect widgets ...
+
+    #
+    # Reload and Test area
+    #
+    reloadCollapsibleButton = ctk.ctkCollapsibleButton()
+    reloadCollapsibleButton.text = "Reload && Test"
+    self.layout.addWidget(reloadCollapsibleButton)
+    reloadFormLayout = qt.QFormLayout(reloadCollapsibleButton)
+
+    # reload button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadButton = qt.QPushButton("Reload")
+    self.reloadButton.toolTip = "Reload this module."
+    self.reloadButton.name = "SequenceResample Reload"
+    reloadFormLayout.addWidget(self.reloadButton)
+    self.reloadButton.connect('clicked()', self.onReload)
+
+    # reload and test button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadAndTestButton = qt.QPushButton("Reload and Test")
+    self.reloadAndTestButton.toolTip = "Reload this module and then run the self tests."
+    reloadFormLayout.addWidget(self.reloadAndTestButton)
+    self.reloadAndTestButton.connect('clicked()', self.onReloadAndTest)
+
+    #
+    # Parameters Area
+    #
+    parametersCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersCollapsibleButton.text = "Parameters"
+    self.layout.addWidget(parametersCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
+
+    #
+    # reference volume selector
+    #
+    self.referenceSelector = slicer.qMRMLNodeComboBox()
+    self.referenceSelector.nodeTypes = ( ("vtkMRMLSequenceNode"), "" )
+    # self.referenceSelector.addAttribute( "vtkMRMLScalarVolumeNode", "LabelMap", 0 )
+    self.referenceSelector.selectNodeUponCreation = True
+    self.referenceSelector.addEnabled = False
+    self.referenceSelector.removeEnabled = False
+    self.referenceSelector.noneEnabled = False
+    self.referenceSelector.showHidden = False
+    self.referenceSelector.showChildNodeTypes = False
+    self.referenceSelector.setMRMLScene( slicer.mrmlScene )
+    self.referenceSelector.setToolTip( "Pick the reference to the algorithm." )
+    
+    parametersFormLayout.addRow("Reference Volume Sequence: ", self.referenceSelector)
+
+    #
+    # input volume selector
+    #
+    self.inputSelector = slicer.qMRMLNodeComboBox()
+    self.inputSelector.nodeTypes = ( ("vtkMRMLScalarVolumeNode"), "" )
+    # self.inputSelector.addAttribute( "vtkMRMLScalarVolumeNode", "LabelMap", 0 )
+    self.inputSelector.selectNodeUponCreation = True
+    self.inputSelector.addEnabled = False
+    self.inputSelector.removeEnabled = False
+    self.inputSelector.noneEnabled = False
+    self.inputSelector.showHidden = False
+    self.inputSelector.showChildNodeTypes = False
+    self.inputSelector.setMRMLScene( slicer.mrmlScene )
+    self.inputSelector.setToolTip( "Pick the input to the algorithm." )
+    
+    parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
+
+    #
+    # resample transform selector
+    #
+    self.transformSelector = slicer.qMRMLNodeComboBox()
+    self.transformSelector.nodeTypes = ( ("vtkMRMLSequenceNode"), "" )
+    self.transformSelector.selectNodeUponCreation = True
+    self.transformSelector.addEnabled = False
+    self.transformSelector.removeEnabled = False
+    self.transformSelector.noneEnabled = False
+    self.transformSelector.showHidden = False
+    self.transformSelector.showChildNodeTypes = False
+    self.transformSelector.setMRMLScene( slicer.mrmlScene )
+    self.transformSelector.setToolTip( "Pick the resample transform to the algorithm." )
+    parametersFormLayout.addRow("Resample Transform Sequence: ", self.transformSelector)
+
+    #
+    # output volume selector
+    #
+    self.outputSelector = slicer.qMRMLNodeComboBox()
+    self.outputSelector.nodeTypes = ( ("vtkMRMLSequenceNode"), "" )
+    # self.outputSelector.addAttribute( "vtkMRMLScalarVolumeNode", "LabelMap", 0 )
+    self.outputSelector.selectNodeUponCreation = False
+    self.outputSelector.addEnabled = True
+    self.outputSelector.removeEnabled = True
+    self.outputSelector.noneEnabled = False
+    self.outputSelector.showHidden = False
+    self.outputSelector.showChildNodeTypes = False
+    self.outputSelector.setMRMLScene( slicer.mrmlScene )
+    self.outputSelector.setToolTip( "Pick the output to the algorithm." )
+    parametersFormLayout.addRow("Output Volume Sequence: ", self.outputSelector)
+
+    #
+    # Apply Button
+    #
+    self.applyButton = qt.QPushButton("Apply")
+    self.applyButton.toolTip = "Run the algorithm."
+    self.applyButton.enabled = False
+    parametersFormLayout.addRow(self.applyButton)
+
+    # connections
+    self.applyButton.connect('clicked(bool)', self.onApplyButton)
+    self.referenceSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.transformSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+
+    # Add vertical spacer
+    self.layout.addStretch(1)
+
+  def cleanup(self):
+    pass
+
+  def onSelect(self):
+    self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode() and self.referenceSelector.currentNode() and self.transformSelector.currentNode()
+
+  def onApplyButton(self):
+    logic = SequenceResampleLogic()
+    print("Run the algorithm")
+    logic.run(self.referenceSelector.currentNode(), self.inputSelector.currentNode(), self.transformSelector.currentNode(), self.outputSelector.currentNode())
+
+  def onReload(self,moduleName="SequenceResample"):
+    """Generic reload method for any scripted module.
+    ModuleWizard will subsitute correct default moduleName.
+    """
+    import imp, sys, os, slicer
+
+    widgetName = moduleName + "Widget"
+
+    # reload the source code
+    # - set source file path
+    # - load the module to the global space
+    filePath = eval('slicer.modules.%s.path' % moduleName.lower())
+    p = os.path.dirname(filePath)
+    if not sys.path.__contains__(p):
+      sys.path.insert(0,p)
+    fp = open(filePath, "r")
+    globals()[moduleName] = imp.load_module(
+        moduleName, fp, filePath, ('.py', 'r', imp.PY_SOURCE))
+    fp.close()
+
+    # rebuild the widget
+    # - find and hide the existing widget
+    # - create a new widget in the existing parent
+    parent = slicer.util.findChildren(name='%s Reload' % moduleName)[0].parent().parent()
+    for child in parent.children():
+      try:
+        child.hide()
+      except AttributeError:
+        pass
+    # Remove spacer items
+    item = parent.layout().itemAt(0)
+    while item:
+      parent.layout().removeItem(item)
+      item = parent.layout().itemAt(0)
+
+    # delete the old widget instance
+    if hasattr(globals()['slicer'].modules, widgetName):
+      getattr(globals()['slicer'].modules, widgetName).cleanup()
+
+    # create new widget inside existing parent
+    globals()[widgetName.lower()] = eval(
+        'globals()["%s"].%s(parent)' % (moduleName, widgetName))
+    globals()[widgetName.lower()].setup()
+    setattr(globals()['slicer'].modules, widgetName, globals()[widgetName.lower()])
+
+  def onReloadAndTest(self,moduleName="SequenceResample"):
+    try:
+      self.onReload()
+      evalString = 'globals()["%s"].%sTest()' % (moduleName, moduleName)
+      tester = eval(evalString)
+      tester.runTest()
+    except Exception, e:
+      import traceback
+      traceback.print_exc()
+      qt.QMessageBox.warning(slicer.util.mainWindow(), 
+          "Reload and Test", 'Exception!\n\n' + str(e) + "\n\nSee Python Console for Stack Trace")
+
+
+#
+# SequenceResampleLogic
+#
+
+class SequenceResampleLogic:
+  """This class should implement all the actual 
+  computation done by your module.  The interface 
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget
+  """
+  def __init__(self):
+    pass
+
+  def hasImageData(self,volumeNode):
+    """This is a dummy logic method that 
+    returns true if the passed in volume
+    node has valid image data
+    """
+    if not volumeNode:
+      print('no volume node')
+      return False
+    if volumeNode.GetImageData() == None:
+      print('no image data')
+      return False
+    return True
+
+  def run(self,referenceSequenceNode, inputNode, transformSequenceNode, outputSequenceNode):
+    """
+    Run the actual algorithm
+    """
+    numOfImageNodes = referenceSequenceNode.GetNumberOfDataNodes()
+
+    for i in xrange(numOfImageNodes):
+      referenceNode = referenceSequenceNode.GetNthDataNode(i)
+      referenceNodeIndexValue = referenceSequenceNode.GetNthIndexValue(i)
+      dimensions = [1,1,1]
+      referenceNode.GetImageData().GetDimensions(dimensions)
+      
+      transformNode = transformSequenceNode.GetNthDataNode(i)
+    
+      inputIJK2RASMatrix = vtk.vtkMatrix4x4()
+      inputNode.GetIJKToRASMatrix(inputIJK2RASMatrix)
+      referenceRAS2IJKMatrix = vtk.vtkMatrix4x4()
+      referenceNode.GetRASToIJKMatrix(referenceRAS2IJKMatrix)
+      inputRAS2RASMatrix = transformNode.GetMatrixTransformToParent()
+    
+      resampleTransform = vtk.vtkTransform()
+      resampleTransform.Identity()
+      resampleTransform.PostMultiply()
+      resampleTransform.SetMatrix(inputIJK2RASMatrix)
+      resampleTransform.Concatenate(inputRAS2RASMatrix) 
+      resampleTransform.Concatenate(referenceRAS2IJKMatrix)
+      resampleTransform.Inverse()
+   
+      resampler = vtk.vtkImageReslice()
+      resampler.SetInput(inputNode.GetImageData())
+      resampler.SetOutputOrigin(0,0,0)
+      resampler.SetOutputSpacing(1,1,1)
+      resampler.SetOutputExtent(0,dimensions[0],0,dimensions[1],0,dimensions[2])
+      resampler.SetResliceTransform(resampleTransform)
+      resampler.Update()
+    
+      outputNode = slicer.vtkMRMLScalarVolumeNode()
+      outputNode.CopyOrientation(referenceNode)
+      outputNode.SetAndObserveImageData(resampler.GetOutput())
+      outputSequenceNode.SetDataNodeAtValue(outputNode, referenceNodeIndexValue)
+    
+    return True
+
+class SequenceResampleTest(unittest.TestCase):
+  """
+  This is the test case for your scripted module.
+  """
+
+  def delayDisplay(self,message,msec=1000):
+    """This utility method displays a small dialog and waits.
+    This does two things: 1) it lets the event loop catch up
+    to the state of the test so that rendering and widget updates
+    have all taken place before the test continues and 2) it
+    shows the user/developer/tester the state of the test
+    so that we'll know when it breaks.
+    """
+    print(message)
+    self.info = qt.QDialog()
+    self.infoLayout = qt.QVBoxLayout()
+    self.info.setLayout(self.infoLayout)
+    self.label = qt.QLabel(message,self.info)
+    self.infoLayout.addWidget(self.label)
+    qt.QTimer.singleShot(msec, self.info.close)
+    self.info.exec_()
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_SequenceResampleTest1()
+
+  def test_SequenceResampleTest1(self):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests sould exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+    #
+    # first, get some data
+    #
+    import urllib
+    downloads = (
+        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
+        )
+
+    for url,name,loader in downloads:
+      filePath = slicer.app.temporaryPath + '/' + name
+      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
+        print('Requesting download %s from %s...\n' % (name, url))
+        urllib.urlretrieve(url, filePath)
+      if loader:
+        print('Loading %s...\n' % (name,))
+        loader(filePath)
+    self.delayDisplay('Finished with download and loading\n')
+
+    volumeNode = slicer.util.getNode(pattern="FA")
+    logic = SequenceResampleLogic()
+    self.assertTrue( logic.hasImageData(volumeNode) )
+    self.delayDisplay('Test passed!')
